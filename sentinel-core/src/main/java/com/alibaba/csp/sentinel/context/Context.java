ba1eba36d00140df6ba647155da7233ff5b1f61f
907764681a <907764681@qq.com>
2025-12-01 17:38:48 +0800

    sentinal bar

---
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/context/Context.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.csp.sentinel.context;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphO;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.node.DefaultNode;
+import com.alibaba.csp.sentinel.node.EntranceNode;
+import com.alibaba.csp.sentinel.node.Node;
+import com.alibaba.csp.sentinel.slots.nodeselector.NodeSelectorSlot;
+
+/**
+ * 该类包含当前调用的元数据：<br/>
+ *
+ * <ul>
+ * <li>{@link EntranceNode}：当前调用的根节点
+ *树。</li>
+ * <li>当前 {@link条目}：当前调用点。</li>
+ * <li>当前 {@link 节点}：与
+ * {@link条目}。</li>
+ * <li>起源：起源在我们想要控制不同时非常有用
+ * 召唤者/消费者分开。通常，来源可能是服务消费者的应用名称
+ * 或原产知识产权。</li>
+ * </ul>
+ * <p>
+ * 每个{@link SphU}#entry（）或{@link SphO}#entry（）都应处于{@link上下文}中，
+ * 如果我们不明确调用{@link ContextUtil}#enter（），则使用默认上下文。
+ * </p>
+ * <p>
+ * 如果我们在 中多次调用 {@link SphU}#entry（） ，则会创建调用树
+ * 同样的语境。
+ * </p>
+ * <p>
+ * 同一资源在不同上下文中将单独计数，参见{@link NodeSelectorSlot}。
+ * </p>
+ *
+ *
+ * @author jialiang.linjl
+ * @author leyou(lihao)
+ * @author Eric Zhao
+ * @see ContextUtil
+ * @see NodeSelectorSlot
+ */
+public class Context {
+
+    /**
+     * Context name.
+     */
+    private final String name;
+
+    /**
+     * The entrance node of current invocation tree.
+     */
+    private DefaultNode entranceNode;
+
+    /**
+     * Current processing entry.
+     */
+    private Entry curEntry;
+
+    /**
+     * The origin of this context (usually indicate different invokers, e.g. service consumer name or origin IP).
+     */
+    private String origin = "";
+
+    private final boolean async;
+
+    /**
+     * Create a new async context.
+     *
+     * @param entranceNode entrance node of the context
+     * @param name context name
+     * @return the new created context
+     * @since 0.2.0
+     */
+    public static Context newAsyncContext(DefaultNode entranceNode, String name) {
+        return new Context(name, entranceNode, true);
+    }
+
+    public Context(DefaultNode entranceNode, String name) {
+        this(name, entranceNode, false);
+    }
+
+    public Context(String name, DefaultNode entranceNode, boolean async) {
+        this.name = name;
+        this.entranceNode = entranceNode;
+        this.async = async;
+    }
+
+    public boolean isAsync() {
+        return async;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Node getCurNode() {
+        return curEntry == null ? null : curEntry.getCurNode();
+    }
+
+    public Context setCurNode(Node node) {
+        this.curEntry.setCurNode(node);
+        return this;
+    }
+
+    public Entry getCurEntry() {
+        return curEntry;
+    }
+
+    public Context setCurEntry(Entry curEntry) {
+        this.curEntry = curEntry;
+        return this;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public Context setOrigin(String origin) {
+        this.origin = origin;
+        return this;
+    }
+
+    public double getOriginTotalQps() {
+        return getOriginNode() == null ? 0 : getOriginNode().totalQps();
+    }
+
+    public double getOriginBlockQps() {
+        return getOriginNode() == null ? 0 : getOriginNode().blockQps();
+    }
+
+    public double getOriginPassReqQps() {
+        return getOriginNode() == null ? 0 : getOriginNode().successQps();
+    }
+
+    public double getOriginPassQps() {
+        return getOriginNode() == null ? 0 : getOriginNode().passQps();
+    }
+
+    public long getOriginTotalRequest() {
+        return getOriginNode() == null ? 0 : getOriginNode().totalRequest();
+    }
+
+    public long getOriginBlockRequest() {
+        return getOriginNode() == null ? 0 : getOriginNode().blockRequest();
+    }
+
+    public double getOriginAvgRt() {
+        return getOriginNode() == null ? 0 : getOriginNode().avgRt();
+    }
+
+    public int getOriginCurThreadNum() {
+        return getOriginNode() == null ? 0 : getOriginNode().curThreadNum();
+    }
+
+    public DefaultNode getEntranceNode() {
+        return entranceNode;
+    }
+
+    /**
+     * Get the parent {@link Node} of the current.
+     *
+     * @return the parent node of the current.
+     */
+    public Node getLastNode() {
+        if (curEntry != null && curEntry.getLastNode() != null) {
+            return curEntry.getLastNode();
+        } else {
+            return entranceNode;
+        }
+    }
+
+    public Node getOriginNode() {
+        return curEntry == null ? null : curEntry.getOriginNode();
+    }
+
+    @Override
+    public String toString() {
+        return "Context{" +
+            "name='" + name + '\'' +
+            ", entranceNode=" + entranceNode +
+            ", curEntry=" + curEntry +
+            ", origin='" + origin + '\'' +
+            ", async=" + async +
+            '}';
+    }
+}
